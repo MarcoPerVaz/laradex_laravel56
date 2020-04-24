233332d84046e74835e09c5a5212a3e7c1961b90
Marco Perdomo <marcooperdomo@gmail.com>
2020-04-24 00:06:37 -0500

    Rutas

--- InstruccionesCommit/Instrucciones_Commit.md
+++ InstruccionesCommit/Instrucciones_Commit.md
@@ -4,41 +4,24 @@
 <!-- end title -->
 
 <!-- commit name -->
-### Commit | __Configuración de nombre, zona horaria y lenguaje de la app__
+### Commit | __Rutas__
 <!-- end commit name -->
 ===
 <!-- official documentation -->
-[Documentación | `Laravel 5.6`](https://laravel.com/docs/5.6)
+[Documentación | `Routing`](https://laravel.com/docs/5.6/routing)
 <!-- end official documentation -->
 
 <!-- commit instructions -->
 #### Instrucciones Commit
-1. Cambiar nombre a la aplicación
-    > php artisan app:name laradex
-2. Edición del archivo de configuración `config\app.php`
-    - Esto: `'timezone' => 'UTC',` por esto: `'timezone' => 'America/Mexico_City',`
-    - Esto: `'locale' => 'en',` por esto: `'locale' => 'es',`
-3. Creación del directorio `resources\lang\es`
-    - Creación y edición del archivo `resources\lang\es\auth.php`
-    - Creación y edición del archivo `resources\lang\es\pagination.php`
-    - Creación y edición del archivo `resources\lang\es\passwords.php`
-    - Creación y edición del archivo `resources\lang\es\validation.php`
-
-      **La información de los 4 archivos fue tomada de [Laravel lang Español](https://github.com/caouecs/Laravel-lang/tree/master/src/es)*
-
-    - Para instalar el paquete de idiomas `Laravel Lang`
-        > composer require caouecs/laravel-lang:~3.0
-
-        **Ir a `vendor/caouecs/laravel-lang` elegir el lenguaje, copiar la carpeta con el idioma que desee y pegarla en `resources\lang`*
+1. Edición del archivo de rutas `routes\web.php`
 <!-- end commit instructions -->
 ===
 <!-- notes -->
 #### Notas:
 - [Documentación oficial | `Laravel 5.6`](https://laravel.com/docs/5.6)
-- [Zonas horarias](https://www.php.net/manual/es/timezones.php)
-- [Laravel lang](https://github.com/caouecs/Laravel-lang)
 <!-- end notes -->
 ===
 <!-- information -->
 #### Información:
+- Más información en `routes\web.php`
 <!-- end information -->
--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,71 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
+/* 
+    | -------------------------------------
+    | *Ruta que Laravel incluye por defecto
+    | *Ruta que apunta a '/' y retorna la vista welcome.blade.php
+    | -------------------------------------
 */
-
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+/* 
+    | -------------------------------------
+    | *Ruta que apunta a '/mi_primer_ruta' y retorna un texto
+    | -------------------------------------
+*/
+Route::get('/mi_primer_ruta', function(){
+    return 'Hello world, esta es mi primer ruta.';
+});
+
+
+/* 
+    | -------------------------------------
+    | *Ruta con parámetros obligatorios que apunta a '/name/{parametro1}/lastname/{parametro2}'
+    |   *Para indicar que un parámetro es obligatorio
+    |       *{parametro}
+    | *Retorna un texto con las variables pasadas como parámetro
+    | -------------------------------------
+*/
+Route::get('/name/{name}/lastname/{lastname}', function($name, $lastname){
+    return "Hola soy " . $name . " " .$lastname;
+});
+
+
+/* 
+    | -------------------------------------
+    | *Ruta con parámetros obligatorios y opcionales que apunta a '/name/{parametro1}/lastname/{parametro2?}'
+    |   *Para indicar que un parámetro es opcional:
+    |       *{parametro2?} Se le agrega ? al parámetro
+    | *Retorna un texto con las variables pasadas como parámetro
+    | -------------------------------------
+*/
+Route::get('/name/{name}/lastname/{lastname?}', function($name, $lastname = null){
+    return "Hola soy " . $name . " " .$lastname;
+});
+
+
+/* 
+    | -------------------------------------------------------------------------------------------------------
+    | *Ruta con parámetros obligatorios y opcionales que apunta a '/name/{parametro1}/lastname/{parametro2?}'
+    |   *Para indicar que un parámetro es opcional:
+    |       *{parametro2?} Se le agrega ? al parámetro
+    | *Para asignar un valor por defecto
+    |   *$lastname = "Apellido"
+    | *Retorna un texto con las variables pasadas como parámetro
+    | -------------------------------------------------------------------------------------------------------
+*/
+Route::get('/name/{name}/lastname/{lastname?}', function($name, $lastname = "Apellido"){
+    return "Hola soy " . $name . " " . $lastname;
+});
+
+
+/* Notas:
+    | -------------------------------------------------------------------------------------------------------
+    | *get() es una petición Http para recuperar datos
+    |   *Más información en https://developer.mozilla.org/es/docs/Web/HTTP/Methods
+    | *Laravel lee las rutas de arriba hacía abajo, por lo que si existen rutas iguales se tomará la de abajo
+    | -------------------------------------------------------------------------------------------------------
+*/
